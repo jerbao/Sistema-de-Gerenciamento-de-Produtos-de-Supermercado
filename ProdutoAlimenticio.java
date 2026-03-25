08a3936106bb179817acaaa1116105fa05d4156a
Jerônimo Bezerra <33181004+jerbao@users.noreply.github.com>
2026-03-25 11:37:09 -0300

    subclasses com regras de preço

---
+++ ProdutoAlimenticio.java
@@ -0,0 +1,26 @@
+public class ProdutoAlimenticio extends Produto {
+
+    private int diasValidade;
+
+    public ProdutoAlimenticio(String nome, int codigoBarras, double precoBase, Fornecedor fornecedor, int diasValidade) {
+        super(nome, codigoBarras, precoBase, fornecedor);
+        this.diasValidade = diasValidade;
+    }
+
+    public int getDiasValidade() {
+        return diasValidade;
+    }
+
+    public void setDiasValidade(int diasValidade) {
+        this.diasValidade = diasValidade;
+    }
+
+    @Override
+    public double calcularPrecoFinal() {
+        double valor = getPrecoBase() * 1.20;//20% de lucro
+        if (diasValidade <= 5) {
+            valor = valor * 0.70;//desconto de 30% para queima de estoque
+        }
+        return valor;
+    }
+}
---
+++ ProdutoEletronico.java
@@ -0,0 +1,26 @@
+public class ProdutoEletronico extends Produto {
+
+    private int mesesGarantia;
+
+    public ProdutoEletronico(String nome, int codigoBarras, double precoBase, Fornecedor fornecedor, int mesesGarantia) {
+        super(nome, codigoBarras, precoBase, fornecedor);
+        this.mesesGarantia = mesesGarantia;
+    }
+
+    public int getMesesGarantia() {
+        return mesesGarantia;
+    }
+
+    public void setMesesGarantia(int mesesGarantia) {
+        this.mesesGarantia = mesesGarantia;
+    }
+
+    @Override
+    public double calcularPrecoFinal() {
+        double valor = getPrecoBase() * 1.50;//50% de margem
+        if (mesesGarantia > 12) {
+            valor = valor * 1.15;//taxa extra de 15% para garantia estendida
+        }
+        return valor;
+    }
+}
